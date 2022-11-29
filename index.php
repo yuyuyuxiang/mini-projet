5833474b9752c6a9decbe658415db466faea4603
Mikhail Trefilov <miketrefiloff@gmail.com>
2022-11-29 13:28:25 +0100

    viewport

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
         <meta charset="utf-8"/>
         <link rel="icon" type="image/x-ico" href="img/logo.png" />
-
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
