0f0a62d8df1e20d7365f4d373f29c8d8bea2940f
yzhang <yuxiangzhang.0521@gmail.com>
2022-12-06 17:56:11 +0100

    petits trucs

--- index.php
+++ index.php
@@ -22,7 +22,6 @@
         <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Chakra+Petch:wght@300;400&family=Righteous&family=Russo+One&display=swap" rel="stylesheet">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-        <script type="text/javascript" src="slick/slick/slick.js"></script>
 
         <?php echo '<link href="'.$css.'.css"'. ' rel="stylesheet" type="text/css">'; ?>
 
