bde5f0dbb1fb9fbbdd59781b2efe7afa12636a37
yzhang <yuxiangzhang.0521@gmail.com>
2023-01-21 00:49:59 +0100

    footer titre

--- footer.php
+++ footer.php
@@ -2,11 +2,11 @@
     <a class="change-color" href="index.php?page=<?php echo $page ?>&css=css0&titre=<?php echo $titre ?>">CSS 0</a>
 </p>
 <p class="footer-ligne">
-    <a class="change-color" href="index.php?page=<?php echo $page ?>&css=css1&titre=<?php echo $titre ?>">CSS 1</a>
+    <a class="change-color" href="index.php?page=<?php echo $page ?>&css=css1&titre=<?php echo $titre ?>">CSS Vide</a>
 </p>
 <p class="footer-ligne">
     <a class="change-color" href="index.php?page=<?php echo $page ?>&css=css2&titre=<?php echo $titre ?>">CSS 2</a>
 </p>
 <p class="footer-ligne">
-    <a class="change-color" href="index.php?page=<?php echo $page ?>&css=css3&titre=<?php echo $titre ?>">CSS 3</a>
+    <a class="change-color" href="index.php?page=<?php echo $page ?>&css=css3&titre=<?php echo $titre ?>">CSS Print</a>
 </p>
